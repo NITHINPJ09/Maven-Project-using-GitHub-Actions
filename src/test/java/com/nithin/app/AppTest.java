5c5d2ec85e408723384c8a137fa02d97504b1524
Nithin Prince John <nithinpj09@gmail.com>
2023-06-14 11:08:31 +0530

    Update AppTest.java

--- src/test/java/com/nithin/app/AppTest.java
+++ src/test/java/com/nithin/app/AppTest.java
@@ -8,7 +8,7 @@ public class AppTest
     @Test
     public void testSum()
     {
-        int expected = 40;
+        int expected = 30;
         int actual = obj.sum(10, 30);
         assertEquals(expected, actual);
     }
